16a6711475004309ef4fdf2530d46169aa4b7f56
Jonathan Law <jonathan.law@hey.com>
2024-11-27 17:37:17 +0000

    Formatting

--- tarotnpc/card.py
+++ tarotnpc/card.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum
 
+
 class Suit(str, Enum):
     WANDS = "wands"
     CUPS = "cups"
     SWORDS = "swords"
     PENTACLES = "pentacles"
+
 
 class Card:
     def __init__(self, description: str, reversed_description: str):
@@ -21,8 +23,11 @@ class MajorArcana(Card):
     def __repr__(self) -> str:
         return self.title.upper()
 
+
 class MinorArcana(Card):
-    def __init__(self, suit: str, value: int, description: str, reversed_description: str):
+    def __init__(
+        self, suit: str, value: int, description: str, reversed_description: str
+    ):
         super().__init__(description, reversed_description)
         self.suit = suit
         self.value = value
